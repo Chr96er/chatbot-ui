3e019d1fc15f690bb67e534bf69c2cacdaaddeab
chr96er <christianhomberg@gmail.com>
2024-05-26 18:46:14 +0200

    Enable dall-e image generation

--- app/api/chat/openai/route.ts
+++ app/api/chat/openai/route.ts
@@ -14,45 +14,117 @@ export async function POST(request: Request) {
     messages: any[]
   }
 
-  try {
-    const profile = await getServerProfile()
+  const latestMessage = messages[messages.length - 1].content
 
-    checkApiKey(profile.openai_api_key, "OpenAI")
+  async function generateImage(
+    latestMessage: string,
+    messages: any[],
+    chatSettings: ChatSettings
+  ) {
+    try {
+      const profile = await getServerProfile()
 
-    const openai = new OpenAI({
-      apiKey: profile.openai_api_key || "",
-      organization: profile.openai_organization_id
-    })
+      checkApiKey(profile.openai_api_key, "OpenAI")
 
-    const response = await openai.chat.completions.create({
-      model: chatSettings.model as ChatCompletionCreateParamsBase["model"],
-      messages: messages as ChatCompletionCreateParamsBase["messages"],
-      temperature: chatSettings.temperature,
-      max_tokens:
-        chatSettings.model === "gpt-4-vision-preview" ||
-        chatSettings.model === "gpt-4o"
-          ? 4096
-          : null, // TODO: Fix
-      stream: true
-    })
+      const openai = new OpenAI({
+        apiKey: profile.openai_api_key || "",
+        organization: profile.openai_organization_id
+      })
 
-    const stream = OpenAIStream(response)
+      messages[messages.length - 1].content =
+        "Take this prompt and turn it into a great prompt to generate a Dall-E image. " +
+        "Make it extremely elaborate and give it plenty of color: \
+        " +
+        latestMessage
 
-    return new StreamingTextResponse(stream)
-  } catch (error: any) {
-    let errorMessage = error.message || "An unexpected error occurred"
-    const errorCode = error.status || 500
+      const imageGenerationPromptStream = await openai.chat.completions.create({
+        model: chatSettings.model as ChatCompletionCreateParamsBase["model"],
+        messages: messages as ChatCompletionCreateParamsBase["messages"],
+        temperature: chatSettings.temperature,
+        max_tokens:
+          chatSettings.model === "gpt-4-vision-preview" ||
+          chatSettings.model === "gpt-4o"
+            ? 4096
+            : null, // TODO: Fix
+        stream: true
+      })
 
-    if (errorMessage.toLowerCase().includes("api key not found")) {
-      errorMessage =
-        "OpenAI API Key not found. Please set it in your profile settings."
-    } else if (errorMessage.toLowerCase().includes("incorrect api key")) {
-      errorMessage =
-        "OpenAI API Key is incorrect. Please fix it in your profile settings."
+      var imageGenerationPrompt = ""
+
+      for await (const chunk of imageGenerationPromptStream) {
+        imageGenerationPrompt += chunk.choices[0]?.delta?.content || ""
+      }
+
+      // Todo CHO20240112: Get those image generation parameters from the UI settings
+      const response = await openai.images.generate({
+        model: "dall-e-3",
+        prompt: latestMessage,
+        n: 1,
+        quality: "hd",
+        response_format: "url"
+      })
+
+      return new Response(
+        imageGenerationPrompt +
+            " \
+        " +
+        response.data[0].url,
+        {
+          status: 200
+        }
+      )
+    } catch (error: any) {
+      const errorMessage =
+        error.error?.message || "An unexpected error occurred"
+      const errorCode = error.status || 500
+      return new Response(JSON.stringify({ message: errorMessage }), {
+        status: errorCode
+      })
     }
+  }
 
-    return new Response(JSON.stringify({ message: errorMessage }), {
-      status: errorCode
-    })
+  async function createCompletion(messages: any[], chatSettings: ChatSettings) {
+    try {
+      const profile = await getServerProfile()
+
+      checkApiKey(profile.openai_api_key, "OpenAI")
+
+      const openai = new OpenAI({
+        apiKey: profile.openai_api_key || "",
+        organization: profile.openai_organization_id
+      })
+
+      const response = await openai.chat.completions.create({
+        model: chatSettings.model as ChatCompletionCreateParamsBase["model"],
+        messages: messages as ChatCompletionCreateParamsBase["messages"],
+        temperature: chatSettings.temperature,
+        max_tokens:
+          chatSettings.model === "gpt-4-vision-preview" ||
+          chatSettings.model === "gpt-4o"
+            ? 4096
+            : null, // TODO: Fix
+        stream: true
+      })
+
+      const stream = OpenAIStream(response)
+
+      return new StreamingTextResponse(stream)
+    } catch (error: any) {
+      const errorMessage =
+        error.error?.message || "An unexpected error occurred"
+      const errorCode = error.status || 500
+      return new Response(JSON.stringify({ message: errorMessage }), {
+        status: errorCode
+      })
+    }
+  }
+
+  if (
+    typeof latestMessage === "string" &&
+    latestMessage.startsWith("Generate an image")
+  ) {
+    return generateImage(latestMessage, messages, chatSettings)
+  } else {
+    return createCompletion(messages, chatSettings)
   }
 }
